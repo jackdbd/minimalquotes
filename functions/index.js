3890ba1b2415446ea9d99c2ac2843102e83b8d66
Giacomo Debidda <jackdebidda@gmail.com>
2020-07-27 16:07:58 +0200

    Add scheduled function for automatic backups on Cloud Storage

--- functions/index.js
+++ functions/index.js
@@ -153,8 +153,42 @@ const mintAdminToken = functions.https.onCall(async (data, context) => {
   return { token };
 });
 
+// TODO: I'm getting 'The caller does not have permission'. I guess I need to
+// configure the permissions on the storage bucket.
+const scheduledFirestoreExport = functions.pubsub
+  .schedule('every 24 hours')
+  .onRun(async (context) => {
+    const firestore = require('@google-cloud/firestore');
+    const client = new firestore.v1.FirestoreAdminClient();
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+    const databaseName = client.databasePath(projectId, '(default)');
+
+    // TODO: avoid hardcoding the storage bucket's name
+    const bucket = 'gs://minimalquotes-5c472.appspot.com';
+
+    // console.log('projectId', projectId);
+    // console.log('databaseName', databaseName);
+
+    try {
+      const responses = await client.exportDocuments({
+        name: databaseName,
+        outputUriPrefix: bucket,
+        // Leave collectionIds empty to export all collections
+        // or set to a list of collection IDs to export,
+        // collectionIds: ['users', 'posts']
+        collectionIds: [],
+      });
+      const response = responses[0];
+      console.log(`Operation Name: ${response['name']}`);
+    } catch (err) {
+      console.error(err);
+      throw new Error('Export operation failed');
+    }
+  });
+
 // exports.addFakeQuoteEveryThreeMinutes = addFakeQuoteEveryThreeMinutes;
-exports.generateFakeQuotes = generateFakeQuotes;
+// exports.generateFakeQuotes = generateFakeQuotes;
 exports.logWhenLoveTagChanges = logWhenLoveTagChanges;
-exports.mintAdminToken = mintAdminToken;
-exports.recursiveDelete = recursiveDelete;
+// exports.mintAdminToken = mintAdminToken;
+// exports.recursiveDelete = recursiveDelete;
+exports.scheduledFirestoreExport = scheduledFirestoreExport;
