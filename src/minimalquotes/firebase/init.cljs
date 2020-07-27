1834b77408990e6f5a321702908562adbcdccaba
Giacomo Debidda <jackdebidda@gmail.com>
2020-07-27 22:27:25 +0200

    Configure Firebase emulator suite

--- src/minimalquotes/firebase/init.cljs
+++ src/minimalquotes/firebase/init.cljs
@@ -29,23 +29,31 @@
     (swap! state/state assoc :firebase-ui ui)
     (swap! state/state assoc :firebase-ui-config ui-config)))
 
-;; TODO: make it generic for dev, staging, production environments.
 (defn init-firebase!
-  "Initialize Firebase auth and Firestore database."
+  "Initialize Firebase app and services."
   []
-  (firebase/initializeApp
-   #js
-    {:apiKey "AIzaSyALIE7Cxbr2X9158aPJtsO_DkwQaPUQqxU",
-     :appId "1:152587589583:web:7bb705180bd399576524ba",
-     :authDomain "minimalquotes-5c472.firebaseapp.com",
-     :databaseURL "https://minimalquotes-5c472.firebaseio.com",
-     :measurementId "G-GHSLN6SEZE",
-     :messagingSenderId "152587589583",
-     :projectId "minimalquotes-5c472",
-     :storageBucket "minimalquotes-5c472.appspot.com"})
-  (firebase/analytics)
-  (firebase/performance)
-  ; (init-firebase-ui! "https://minimalquotes-5c472.web.app/")
-  (init-firebase-ui! "http://localhost:3000/")
-  (reset! state/db (firebase/firestore))
+  (let [app (firebase/initializeApp
+             #js
+              {:apiKey "AIzaSyALIE7Cxbr2X9158aPJtsO_DkwQaPUQqxU",
+               :appId "1:152587589583:web:7bb705180bd399576524ba",
+               :authDomain "minimalquotes-5c472.firebaseapp.com",
+               :databaseURL "https://minimalquotes-5c472.firebaseio.com",
+               :measurementId "G-GHSLN6SEZE",
+               :messagingSenderId "152587589583",
+               :projectId "minimalquotes-5c472",
+               :storageBucket "minimalquotes-5c472.appspot.com"})]
+    (if (= "localhost" (.. js/window -location -hostname))
+      (do (prn (str "Firebase app " (.-name app) " is running locally"))
+          (prn "Configure Functions emulator")
+          (doto (.functions app)
+           (.useFunctionsEmulator "http://localhost:5001"))
+          (init-firebase-ui! "http://localhost:3000/")
+          (prn "Configure Firestore emulator")
+          (reset! state/db
+                  (doto (firebase/firestore)
+                   (.settings #js {:host "localhost:8080", :ssl false}))))
+      (do (firebase/analytics)
+          (firebase/performance)
+          (init-firebase-ui! "https://minimalquotes-5c472.web.app/")
+          (reset! state/db (firebase/firestore)))))
   (on-auth-state-changed))
