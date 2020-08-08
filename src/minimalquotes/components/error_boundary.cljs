f268f5e22af95dfcae7602662661aab9d5ddadd6
Giacomo Debidda <jackdebidda@gmail.com>
2020-08-08 12:07:04 +0200

    Better error stack traces in error boundary with stacktrace.js

--- src/minimalquotes/components/error_boundary.cljs
+++ src/minimalquotes/components/error_boundary.cljs
@@ -1,38 +1,62 @@
 (ns minimalquotes.components.error-boundary
-  (:require [minimalquotes.components.buttons :as btn]
-            [minimalquotes.utils :refer [log-error]]
-            [reagent.core :as r]))
+  (:require
+    [goog.object :as object]
+    [minimalquotes.components.buttons :as btn]
+    [minimalquotes.utils :refer [log-error]]
+    [reagent.core :as r]
+    ["stacktrace-js" :as StackTrace]))
 
-; https://tailwindcss.com/components/alerts/#app
+(defn s->li
+  [i s]
+  ^{:key i} [:li {:class "list-decimal"} s])
+
 (defn fallback-ui
-  [{:keys [detail on-click]}]
+  "Component to show in place of a component tree whenever a error boundary
+  catches an error.
+  Styling taken from here: https://tailwindcss.com/components/alerts/#app"
+  [{:keys [component-stack error-message on-click stack-frames]}]
   [:div {:role :alert}
    [:div {:class ["bg-red-500 rounded-t px-4 py-2"]}
     [:h2 {:class ["text-white text-2xl"]} "Ops. Something went wrong."]]
-   [:div
-    {:class
-     ["border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700"]}
-    [:p detail]] [btn/button {:on-click on-click :text "Try again"}]])
+   [:div {:class ["border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700"]}
+    [:h3 {:class ["text-xl"]} error-message]
+    [:details
+     [:summary "Component Stack"]
+     component-stack]
+    [:details
+     [:summary "Stack Frames"]
+     [:ul {:class ["pt-2" "pl-4"]}
+      (map-indexed s->li stack-frames)]]]
+   [btn/button {:on-click on-click :text "Try again"}]])
 
 (defn error-boundary
-  "TODO: create nicer error messages in this error boundary."
-  [component-tree]
+  "React error boundary."
+  [{:keys [on-catch]} _]
   (let [error (r/atom nil)
-        detail (r/atom "")]
+        component-stack (r/atom "")
+        stack-frames (r/atom [])
+        on-stackframes (fn [^js stackframes]
+                         (let [sf->str (fn [^js sf]
+                                         ;  (prn "StackFrame properties"
+                                         ;  (object/getAllPropertyNames sf))
+                                         ;  (prn (js->clj sf))
+                                         (.toString sf))]
+                           (.forEach stackframes #(swap! stack-frames conj (sf->str %)))))]
     (r/create-class
-      {:component-did-catch
-       (fn [_ ^js err ^js info]
-         (log-error err)
-         (reset! detail info)
-         (js/console.warn "Todo: send error to Firebase Crashlytics or Sentry"
-                          "info"
-                          info))
-       :get-derived-state-from-error (fn [e] (reset! error e) #js {})
-       :reagent-render (fn [component-tree]
+      {:component-did-catch (fn [_ err ^js info]
+                              (reset! component-stack (object/getValueByKeys info #js ["componentStack"]))
+                              ;  (prn "err properties"
+                              ;  (object/getAllPropertyNames err))
+                              (-> (StackTrace/fromError err)
+                                  (.then on-stackframes)
+                                  (.catch log-error))
+                              (on-catch err info))
+       :get-derived-state-from-error (fn [err]
+                                       (reset! error err))
+       :reagent-render (fn [_ component-tree]
                          (if @error
-                           [fallback-ui
-                            {:detail (goog.object/getValueByKeys
-                                       @detail
-                                       #js ["componentStack"])
-                             :on-click #(reset! error nil)}]
+                           [fallback-ui {:component-stack @component-stack
+                                         :error-message (.. @error -message)
+                                         :on-click #(reset! error nil)
+                                         :stack-frames @stack-frames}]
                            component-tree))})))
--- src/minimalquotes/components/error_boundary_cards.cljs
+++ src/minimalquotes/components/error_boundary_cards.cljs
@@ -1,32 +1,43 @@
 (ns minimalquotes.components.error-boundary-cards
-  (:require [devcards.core :as dc :refer [defcard]]
-            [minimalquotes.components.buttons :as btn]
-            [minimalquotes.components.error-boundary :refer [error-boundary]]
-            [reagent.core :as r]))
+  (:require
+    [devcards.core :as dc :refer [defcard]]
+    [goog.object :as object]
+    [minimalquotes.components.buttons :as btn]
+    [minimalquotes.components.error-boundary :refer [error-boundary]]
+    [reagent.core :as r]))
 
 (defn throw-in-3-seconds
+  "Component that always throws after 3 seconds it was mounted in the DOM."
   []
   (let [seconds-elapsed (r/atom 0)]
     (fn []
       (js/setTimeout #(swap! seconds-elapsed inc) 1000)
-      (when (= 3 @seconds-elapsed) (throw (js/Error. "Boom")))
+      (when (= 3 @seconds-elapsed)
+        (throw (js/Error. "Boom (this is the error message)")))
       [:div "Throwing error in " (- 3 @seconds-elapsed) " seconds"])))
 
 (defcard "# Error Boundary")
 
-(defcard
-  "An error boundary catches JavaScript errors anywhere in its child
-          component tree, logs those errors, and displays a fallback UI instead
+(defcard "An error boundary catches JavaScript errors anywhere in its child 
+          component tree, logs those errors, and displays a fallback UI instead 
           of the component tree that crashed.")
 
-(defcard error-boundary-card (dc/reagent [error-boundary [throw-in-3-seconds]]))
+(defn on-catch
+  [^js err ^js info]
+  (let [component-stack (object/getValueByKeys info #js ["componentStack"])
+        error-stack (object/getValueByKeys err #js ["stack"])]
+    (js/console.warn "TODO: send to Sentry" "component-stack" component-stack)
+    (js/console.warn "TODO: send to Sentry" "error-stack" error-stack)))
 
-(defcard
-  "An error boundary does not catch errors inside event handlers
-  https://reactjs.org/docs/error-boundaries.html#how-about-event-handlers")
+(defcard error-boundary-card
+  (let [props {:on-catch on-catch}]
+    (dc/reagent [error-boundary props
+                 [throw-in-3-seconds]])))
+
+(defcard "React [Error Boundaries](https://reactjs.org/docs/error-boundaries.html#how-about-event-handlers) do not catch errors inside event handlers.")
 
 (defcard error-boundary-event-handler-card
-         (dc/reagent [error-boundary
-                      [btn/button
-                       {:on-click #(throw (js/Error. "Boom"))
-                        :text "Click to throw (but not catch)"}]]))
+  (let [props {:on-catch on-catch}]
+    (dc/reagent [error-boundary props
+                 [btn/button {:on-click #(throw (js/Error. "Boom"))
+                              :text "Click to throw (but not catch)"}]])))
