202756ac443e0bb6a4228c30c22643de94e02e1b
Giacomo Debidda <jackdebidda@gmail.com>
2020-08-17 22:19:59 +0200

    Improve infinite scrolling
    When the Firestore query is ready, stop showing the spinner.
    Show another component instead.

--- src/minimalquotes/components/observed_spinner.cljs
+++ src/minimalquotes/components/observed_spinner.cljs
@@ -1,6 +1,7 @@
 (ns minimalquotes.components.observed-spinner
   (:require
     [goog.object :as object]
+    [lambdaisland.glogi :as log]
     [minimalquotes.components.icons :refer [icon-spinner]]
     [minimalquotes.components.intersection-observer :refer [intersection-observer]]
     [minimalquotes.firebase.firestore :refer [query update-state-from-firestore!]]
@@ -12,27 +13,11 @@
    https://firebase.google.com/docs/reference/js/firebase.firestore.QueryDocumentSnapshot"
   [^js query-document-snapshot]
   (when query-document-snapshot
-    ; (prn "each id" (.-id query-document-snapshot))
     (update-state-from-firestore! {:ratom state/quotes} query-document-snapshot)))
-
-(defn on-first-snapshot
-  "Callback to invoke for the first QueryDocumentSnapshot."
-  [^js query-document-snapshot]
-  (when query-document-snapshot
-    ;; (prn "first id" (.-id query-document-snapshot))
-    (reset! state/first-quote query-document-snapshot)))
-
-(defn on-last-snapshot
-  "Callback to invoke for the last QueryDocumentSnapshot."
-  [^js query-document-snapshot]
-  (when query-document-snapshot
-    ;; (prn "last id" (.-id query-document-snapshot))
-    (reset! state/last-quote query-document-snapshot)))
 
 ;; TODO: how do I know when to stop showing the spinner? Let's say I have 100
 ;; documents in a Firestore collection, and I fetched all of them. How do I
-;; know
-;; that I actually fetched all the documents?
+;; know that I actually fetched all the documents?
 
 (defn observed-spinner
   "Stateful component that fetches data from Firestore when the spinner within
@@ -43,29 +28,40 @@
   component unmounts.
   https://stackoverflow.com/questions/28888340/should-clojure-core-async-channels-be-closed-when-not-used-anymore"
   [{:keys [root-id]}]
-  (let [state (r/atom {})
+  (let [local-state (r/atom {:fetching false
+                             :root nil
+                             :unobserve nil})
+        on-first-snapshot (fn [^js query-document-snapshot]
+                            (when query-document-snapshot
+                              (reset! state/first-quote query-document-snapshot)))
+        on-last-snapshot (fn [^js query-document-snapshot]
+                           (swap! local-state assoc :fetching false)
+                           (when query-document-snapshot
+                             (log/debug :fetch-from-firestore {:message (str "last doc id: " (.-id query-document-snapshot))})
+                             (reset! state/last-quote query-document-snapshot)))
         did-mount (fn [_]
                     (let [root (js/document.querySelector (str "#" root-id))]
-                      (swap! state assoc :root root)))
+                      (swap! local-state assoc :root root)))
         will-unmount (fn [_]
-                       (let [unobserve (:unobserve @state)]
+                       (let [unobserve (:unobserve @local-state)]
                          (unobserve)))
         on-change (fn [^js entry ^js unobserve]
                     ;; (prn "getAllPropertyNames entry"
                     ;; (object/getAllPropertyNames entry false false))
-                    (swap! state assoc :unobserve unobserve)
+                    (swap! local-state assoc :unobserve unobserve)
                     (let [m {:on-each-snapshot on-each-snapshot
                              :on-first-snapshot on-first-snapshot
                              :on-last-snapshot on-last-snapshot}
                           is-intersecting (object/get entry "isIntersecting")
                           ratio (object/get entry "intersectionRatio")]
                       (when (and is-intersecting (> ratio 0.5))
-                        (prn "=== try fetching next batch of results ===")
+                        (log/debug :on-change {:message "Fetch next batch of documents from Firestore"})
+                        (swap! local-state assoc :fetching true)
                         (query @state/db "quotes" m {:limit 20
                                                      :order-by [["createdAt"]]
                                                      :start-after @state/last-quote}))))
         reagent-render (fn []
-                         [intersection-observer {:root (:root @state)
+                         [intersection-observer {:root (:root @local-state)
                                                  :threshold 0.5
                                                  :onChange on-change}
                           ;; The purpose of the padding p-8 is twofold:
@@ -74,8 +70,10 @@
                           ;; 2) to leave some space for the bounding box of the
                           ;;    SVG circle and path, so they don't interfere
                           ;;    with the root element's scrolling behavior.
-                          [:div {:class ["flex" "items-center" "justify-center" "p-8"]}
-                           [icon-spinner {:size 24 :text-color "text-purple-600"}]]])]
+                          [:div {:class ["flex" "items-center" "justify-center" "h-24" "p-8"]}
+                           (if (get @local-state :fetching)
+                             [icon-spinner {:size 16 :text-color "text-purple-600"}]
+                             [:div "No more results"])]])]
     (r/create-class {:component-did-mount did-mount
                      :component-will-unmount will-unmount
                      :display-name "observed-spinner"
